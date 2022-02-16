ad1ea5b4d1a3fac5dac19f05ffc7b3594f1ffa58
junaidtariq-te <junaid.t@theentertainerasia.com>
2022-02-16 14:57:22 +0500

    :relieved: flask app second commit. (completed)

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return 'Hello Junaid!'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
